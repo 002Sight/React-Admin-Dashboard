dfa6fbec7090d22caf0f8a864d498d0df218267e
002Sight <112473993+002Sight@users.noreply.github.com>
2022-08-31 00:53:29 +0300

    Update App.js

--- react-deploy-admdashboard/src/App.js
+++ react-deploy-admdashboard/src/App.js
@@ -1,7 +1,25 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { FiSettings } from "react-icons/fi"
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import "./App.css";
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from react</h1>
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4 style={{ zIndex: '1000'}} ">
+            <TooltipComponent content="Settings" position="Top">
+            <button>
+              <FiSettings/>
+            </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
