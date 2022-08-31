ed92b3dbaf8bc06ed355f7d7b0249ff1acd1e26e
002Sight <112473993+002Sight@users.noreply.github.com>
2022-08-31 22:09:33 +0300

    some changes

--- react-deploy-admdashboard/src/App.js
+++ react-deploy-admdashboard/src/App.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from "react";
+import React, {  useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { FiSettings } from "react-icons/fi"
 import { DialogButtonDirective, TooltipComponent } from "@syncfusion/ej2-react-popups";
@@ -10,6 +10,9 @@ import {
   Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar,
   Pie, Financial, ColorPicker, ColorMapping, Editor, Line
 } from "./pages"
+
+import { useStateContext } from "./contexts/ContextProvider";
+
 import "./App.css";
 
 
